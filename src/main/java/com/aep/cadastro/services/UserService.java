8de3c7a1e50bacea811b688a650699542e16381b
Nicolas Gomes <Nicolasgomescosta@outlook.com>
2026-09-07 17:07:23 -0300

    Update UserService.java
    Regra mínima para criação de usuários no banco

--- src/main/java/com/aep/cadastro/services/UserService.java
+++ src/main/java/com/aep/cadastro/services/UserService.java
@@ -26,6 +26,13 @@ public class UserService {
     }
 
     public UserModel createUser(UserModel user){
+        if (user.getNome() == null) {
+            throw new IllegalArgumentException("Nomé é Obrigatorio");
+        }
+        if (user.getIdade() <= 0) {
+            throw new IllegalArgumentException("Idade deve ser maior que zero");
+        }
+        
         return userRepository.save(user);
     }
 
