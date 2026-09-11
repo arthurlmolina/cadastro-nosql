eee6bb3e33860c9ad0e4a5402141eff56492cd73
Nicolas Gomes <Nicolasgomescosta@outlook.com>
2026-09-10 21:46:16 -0300

    Update CadastroApplicationTests.java
    jesus atenda minhas preçes

--- src/test/java/com/aep/cadastro/CadastroApplicationTests.java
+++ src/test/java/com/aep/cadastro/CadastroApplicationTests.java
@@ -17,4 +17,33 @@ class CadastroApplicationTests {
 
         CadastroApplication.main(new String[]{});
     }
+
+	@Test
+	void deveVerVagasEVoltar() {
+
+			String entrada =
+				"1\n" + " 0\n" + "0\n";
+
+		System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+		CadastroApplication.main(new String[]{});
+	}
+
+	@Test
+	void informaVagaInvalida() {
+
+		String entrada = 
+			"1\n" + "s\n" + "0\n";
+		System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+		CadastroApplication.main(new String[]{});
+		
+ 	}
+
+	@Test 
+	void realizarCandidatura(){
+		String entrada = 
+			"1\n" + "1\n" + "João\n" + "20\n" + "Tenho interesse\n" + "0\n";
+		System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+		CadastroApplication.main(new String[]{});
+	}
+
 }
