408623c135ec36bdfeeb0a42374b483fa44e876b
Nicolas Gomes <Nicolasgomescosta@outlook.com>
2026-09-10 22:05:16 -0300

    Update CadastroApplicationTests.java
    Curriculos

--- src/test/java/com/aep/cadastro/CadastroApplicationTests.java
+++ src/test/java/com/aep/cadastro/CadastroApplicationTests.java
@@ -64,4 +64,32 @@ class CadastroApplicationTests {
 		CadastroApplication.main(new String[]{});
 	}
 
+	@Test
+	void gerenciarCurriculosEVoltar() {
+		String entrada = "1\n" + "1\n" + "João\n" + "20\n" + "Obs Teste\n" + "2\n" + "0\n" + "0\n";
+
+		System.setIn(
+			new ByteArrayInputStream(entrada.getBytes()));
+			CadastroApplication.main(new String[]{});
+	}
+
+	@Test
+	void editarCurriculo() {
+		String entrada = "1\n" + "1\n" + "João\n" + "20\n" + "Observacao inicial\n" +
+            "2\n" + "1\n" + "1\n" + "João Silva\n" + "21\n" + "Observacao atualizada\n" + "0\n";
+		System.setIn(
+			new ByteArrayInputStream(entrada.getBytes()));
+			CadastroApplication.main(new String[]{});
+	}
+
+	@Test
+	void deletarCurriculo() {
+		String entrada = "1\n" +"1\n" + "Maria\n" + "25\n" + "Curriculo para exclusao\n" +
+			"2\n" + "2\n" + "1\n" + "S\n" + "0\n";
+		System.setIn(
+			new ByteArrayInputStream(entrada.getBytes())
+		);
+		CadastroApplication.main(new String[]{});
+	}
+
 }
